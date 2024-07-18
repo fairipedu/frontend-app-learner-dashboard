0ec4407b28602d29b9578eb6518ff16ba1ad55cf
fairipedu <fairipdev01@gmail.com>
2024-07-18 12:43:00 +0900

    error fix

--- src/components/Accodion/AccordionItem.jsx
+++ src/components/Accodion/AccordionItem.jsx
@@ -9,17 +9,24 @@ const AccordionItem = (props) => {
   return (
     <div className="rc-accordion-card">
       <div className="rc-accordion-header">
-        <div className={`rc-accordion-toggle p-3 ${active === id ? 'active' : ''}`} onClick={() => handleToggle(id)}>
+        <div className={`rc-accordion-toggle p-3 ${active === id ? 'active' : ''}`}
+          onClick={() => handleToggle(id)}
+          aria-hidden="true"
+        >
           { /* subNav.length > 0 */}
           <h5 className="rc-accordion-title"><i className={icon}></i> &nbsp; {header}</h5>
           <i className="ri-arrow-down-s-line arrow"></i>
-          </div>
         </div>
-        <div ref={contentEl} className={`rc-collapse ${active === id ? 'show' : ''}`} style={
+      </div>
+      <div
+        ref={contentEl}
+        className={`rc-collapse ${active === id ? 'show' : ''}`}
+        style={
           active === id
           ? { height: contentEl.current.scrollHeight }
-          : { height: "0px" }
-        }>
+          : { height: '0px' }
+        }
+      >
         <div className="rc-accordion-body">
           <p className="mb-0">{subNav}</p>
         </div>
@@ -27,11 +34,14 @@ const AccordionItem = (props) => {
     </div>
   )
 };
+AccordionItem.defaultProps = {
+  props: null,
+};
 AccordionItem.propsTypes = {
   props: PropTypes.shape({
     handdleToggle: PropTypes.string,
     active: PropTypes.bool,
-    subNavList: PropTypes.object,
+    subNavList: PropTypes.shape,
   })
 };
 export default AccordionItem;
