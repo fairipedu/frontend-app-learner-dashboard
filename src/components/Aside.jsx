3d9a16e9c2f46d1379f5c2b6e738651c0085b636
fairipedu <fairipdev01@gmail.com>
2024-07-18 11:37:22 +0900

    error fix

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -5,11 +5,11 @@ import AccordionItem from './Accodion/AccordionItem';
 
 export const Aside = () => {
   const asideList = [
-    { id: 1, header : '대시보드', icon : 'ri-home-5-fill', subNav : [] },
-    { id: 2, header : '교과 과정', icon : 'ri-book-2-fill', subNav : ['나의강좌', '청강신청'] },
-    { id: 3, header : '비교과 과정', icon : 'ri-book-shelf-fill', subNav : ['나의강좌', '강좌목록'] },
-    { id: 4, header : '자율강좌', icon : 'ri-book-open-fill', subNav : [] },
-    { id: 5, header : '이용안내', icon : 'ri-chat-smile-fill', subNav : [] }
+    { id: 1, header: '대시보드', icon: 'ri-home-5-fill', subNav: [] },
+    { id: 2, header: '교과 과정', icon: 'ri-book-2-fill', subNav: ['나의강좌', '청강신청'] },
+    { id: 3, header: '비교과 과정', icon: 'ri-book-shelf-fill', subNav: ['나의강좌', '강좌목록'] },
+    { id: 4, header: '자율강좌', icon: 'ri-book-open-fill', subNav: [] },
+    { id: 5, header: '이용안내', icon: 'ri-chat-smile-fill', subNav: [] }
   ];
   const [active, setActive] = useState(null);
 
