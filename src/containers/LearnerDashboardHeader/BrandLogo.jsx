d3e7faab44bbd9733a8adf254efba7e895a50b18
fairipedu <fairipdev01@gmail.com>
2024-10-04 11:55:06 +0900

    css 수정

--- src/containers/LearnerDashboardHeader/BrandLogo.jsx
+++ src/containers/LearnerDashboardHeader/BrandLogo.jsx
@@ -6,6 +6,8 @@ import { reduxHooks } from 'hooks';
 
 import messages from './messages';
 
+import { getConfig } from '@edx/frontend-platform';
+
 export const BrandLogo = () => {
   const { formatMessage } = useIntl();
   const dashboard = reduxHooks.useEnterpriseDashboardData();
@@ -14,7 +16,7 @@ export const BrandLogo = () => {
     <a href={dashboard?.url || '/'} className="logo-wrap">
       <img
         className="logo"
-        src="../logo_white.png"
+        src={`${getConfig().LMS_BASE_URL}/theming/asset/images/theme_logo_white.png`}
         alt={formatMessage(messages.logoAltText)}
       />
     </a>
